18f36101d173e253c282129d1168c38dbbae7b3b
pancaholic <putrapanca21@gmail.com>
2024-04-03 21:15:27 +0700

    auth-layout config router

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import FormLogin from "../Fragments/FormLogin";
+import { Link } from "react-router-dom";
 
 const AuthLayouts = (props) => {
-  const { children, title } = props;
+  const { children, title, type } = props;
+
+  const Navigation = ({ type }) => {
+    if (type === "login") {
+      return (
+        <p className="mt-5 text-sm text-center">
+          Don't have an account?{" "}
+          <Link to="/register" className="text-blue-600 font-bold">
+            Register
+          </Link>
+        </p>
+      );
+    } else {
+      return (
+        <p className="mt-5 text-sm text-center">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-600 font-bold">
+            Login
+          </Link>
+        </p>
+      );
+    }
+  };
+
   return (
     <div className="flex justify-center bg-white items-center min-h-screen">
       <div className="w-full max-w-xs">
@@ -11,6 +35,7 @@ const AuthLayouts = (props) => {
           Welcome, Please enter your details
         </p>
         {children}
+        <Navigation type={type} />
       </div>
     </div>
   );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,14 +5,8 @@ import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
-    <AuthLayouts title="Login">
+    <AuthLayouts title="Login" type="login">
       <FormLogin />
-      <p className="mt-5 text-sm text-center">
-        Don't have an account?{" "}
-        <Link to="/register" className="text-blue-600 font-bold">
-          Register
-        </Link>
-      </p>
     </AuthLayouts>
   );
 };
