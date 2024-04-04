4d42755d1c693abc9ad856885a654ad4d9550252
pancaholic <putrapanca21@gmail.com>
2024-04-04 10:44:35 +0700

    dashboard page

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Button from "../components/Elements/Button";
+
+const DashboardPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col items-center">
+        <h1 className="text-3xl font-bold text-green-700 mb-5">
+          {" "}
+          Welcome to Dashboard
+        </h1>
+        <Button
+          variant="bg-blue-700"
+          onClick={() => (window.location.href = "/login")}
+        >
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
