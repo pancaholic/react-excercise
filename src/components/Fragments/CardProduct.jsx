7460dda4e8c5ebc3183f0a16c9c45044b63c0b3c
pancaholic <putrapanca21@gmail.com>
2024-04-17 12:18:32 +0700

    get Data API

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -5,20 +5,20 @@ const Header = (props) => {
   const { image } = props;
   return (
     <a href="#" alt="product">
-      <img src={image} alt="" className="p-3 rounded-t-2xl" />
+      <img src={image} alt="" className="p-3 rounded-t-2xl h-60 w-full object-cover" />
     </a>
   );
 };
 
 const Body = (props) => {
-  const { children, name } = props;
+  const { children, title } = props;
   return (
-    <div className="px-8 pb-5 h-full">
+    <div className="px-8 h-full">
       <a href="#">
-        <h5 className="text-xl font-semibold tracking-tight text-white">
-          {name}
+        <h5 className="text-xl font-bold tracking-tight text-yellow-300">
+          {title.substring(0, 20)} ...
         </h5>
-        <p className="text-white text-justify">{children}</p>
+        <p className="text-white text-justify">{children.substring(0, 100)}</p>
       </a>
     </div>
   );
@@ -26,14 +26,14 @@ const Body = (props) => {
 
 const Footer = (props) => {
   const { price, addToCart, id } = props;
-  let idr = new Intl.NumberFormat("id-ID", {
+  let usd = new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: "IDR",
+    currency: "USD",
     minimumFractionDigits: 0,
   });
   return (
     <div className="flex justify-between items-center px-8 pb-8 md:px-3">
-      <span className="text-white text-xl font-bold">{idr.format(price)}</span>
+      <span className="text-white text-xl font-bold">{usd.format(price)}</span>
       <Button variant="bg-yellow-500" onClick={() => addToCart(id)}>
         Add to Cart
       </Button>
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,86 +1,49 @@
 import { useEffect, useRef, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import Button from "../components/Elements/Button";
-
-const products = [
-  {
-    id: 1,
-    name: "Jordan 1 Low Mocha",
-    price: 1000000,
-    image: "/images/jordan-low.jpg",
-    description: `Trade? Never. Trophy Room is 
-    back with an AJ1 inspired by MJ's most coveted rookie card. The GOAT's embroidered signature graces the outer heel,
-    while a tonal Trophy Room logo and polished Wings logo create the co-branded bliss. Premium materials and special 
-    edition packaging bring this pair back to the '85 season, so we can all relive the unforgettable journey together. `,
-  },
-  {
-    id: 2,
-    name: "Jordan 1 Low Bred",
-    price: 2000000,
-    image: "/images/jordan-low.jpg",
-    description: `Trade? Never. Trophy Room is 
-    onal Trophy Room logo and polished Wings logo create the co-branded bliss. Premium materials and special 
-    edition packaging bring this pair back to the '85 season, so we can all relive the unforgettable journey together. `,
-  },
-  {
-    id: 3,
-    name: "Jordan 1 Low Panda",
-    price: 2500000,
-    image: "/images/jordan-low.jpg",
-    description: `Trade? Never. Trophy Room is 
-     this pair back to the '85 season, so we can all relive the unforgettable journey together. `,
-  },
-  {
-    id: 4,
-    name: "Jordan 1 Low x Dior",
-    price: 100000000,
-    image: "/images/jordan-low.jpg",
-    description: `Trade? Never. Trophy Room is 
-    back with an AJ1 inspired by MJ's most coveted rookie card. The GOAT's embroidered signature graces the outer heel,
-   ging bring this pair back to the '85 season, so we can all relive the unforgettable journey together. `,
-  },
-  {
-    id: 5,
-    name: "Jordan 1 Low OG Travis Scott",
-    price: 20000000,
-    image: "/images/jordan-low.jpg",
-    description: `Trade? Never. Trophy Room is 
-    back with an AJ1 inspired by MJ's most coveted rookie card. The GOAT's embroidered signature graces the outer heel,
-    while a ng bring this pair back to the '85 season, so we can all relive the unforgettable journey together. `,
-  },
-  {
-    id: 6,
-    name: "Jordan 1 Low Union",
-    price: 27000000,
-    image: "/images/jordan-low.jpg",
-    description: `Trade? Never. Trophy Room is 
-   onal Trophy Room logo and polished Wings logo create the co-branded bliss. Premium materials and special 
-    edition packaging bring this pair back to the '85 season, so we can all relive the unforgettable journey together. `,
-  },
-];
+import { getProducts } from "../services/product.service";
 
 const email = localStorage.getItem("email");
 
 const ProductPage = () => {
   const [cart, setCart] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [products, setProducts] = useState([]);
 
+  // useRef
+  const cartRef = useRef(JSON.parse(localStorage.getItem("cart")) || []);
+
+  const handleAddToCartRef = (id) => {
+    cartRef.current = [...cartRef.current, { id, qty: 1 }];
+    localStorage.setItem("cart", JSON.stringify(cartRef.current));
+  };
+
+  // Menampilkan total price jika sudah add to cart
+  const totalPriceRef = useRef(null);
   useEffect(() => {
     setCart(JSON.parse(localStorage.getItem("cart")) || []);
   }, []);
 
+  // Untuk Cart
   useEffect(() => {
-    if (cart.length > 0) {
+    if (products.length > 0 && cart.length > 0) {
+      totalPriceRef.current.style.display = "table-row";
       const sum = cart.reduce((acc, item) => {
         const product = products.find((product) => product.id === item.id);
         return acc + product.price * item.qty;
       }, 0);
       setTotalPrice(sum);
       localStorage.setItem("cart", JSON.stringify(cart));
+    } else {
+      totalPriceRef.current.style.display = "none";
     }
-  }, [cart]);
+  }, [cart, products]);
 
-
+  useEffect(() => {
+    getProducts((data) => {
+      setProducts(data);
+    });
+  }, []);
 
   const handleAddToCart = (id) => {
     if (cart.find((item) => item.id === id)) {
@@ -106,25 +69,6 @@ const ProductPage = () => {
     window.location.href = "/product";
   };
 
-  // useRef
-  const cartRef = useRef(JSON.parse(localStorage.getItem("cart")) || []);
-
-  const handleAddToCartRef = (id) => {
-    cartRef.current = [...cartRef.current, { id, qty: 1 }];
-    localStorage.setItem("cart", JSON.stringify(cartRef.current));
-  };
-
-  // Menampilkan total price jika sudah add to cart
-  const totalPriceRef = useRef(null);
-
-  useEffect(() => {
-    if(cart.length > 0) {
-      totalPriceRef.current.style.display = "table-row";
-    }else{
-      totalPriceRef.current.style.display = "none";
-    }
-  }, [cart])
-
   return (
     <>
       <div className="flex justify-end h-20 bg-blue-700 text-white items-center px-10">
@@ -135,19 +79,20 @@ const ProductPage = () => {
       </div>
       <div className="flex justify-center py-5">
         <div className="w-3/4 flex flex-wrap">
-          {products.map((product) => (
-            <CardProduct key={product.id}>
-              <CardProduct.Header image={product.image} />
-              <CardProduct.Body name={product.name}>
-                {product.description}
-              </CardProduct.Body>
-              <CardProduct.Footer
-                price={product.price}
-                id={product.id}
-                addToCart={handleAddToCart}
-              />
-            </CardProduct>
-          ))}
+          {products.length > 0 &&
+            products.map((product) => (
+              <CardProduct key={product.id}>
+                <CardProduct.Header image={product.image} />
+                <CardProduct.Body title={product.title}>
+                  {product.description}
+                </CardProduct.Body>
+                <CardProduct.Footer
+                  price={product.price}
+                  id={product.id}
+                  addToCart={handleAddToCart}
+                />
+              </CardProduct>
+            ))}
         </div>
         <div className="w-1/4 flex flex-col">
           <h1 className="text-2xl font-bold text-blue-700 mb-2">Cart</h1>
@@ -161,37 +106,38 @@ const ProductPage = () => {
               </tr>
             </thead>
             <tbody>
-              {cart.map((item) => {
-                const product = products.find((p) => p.id === item.id);
-                return (
-                  <tr key={item.id}>
-                    <td>{product.name}</td>
-                    <td>
-                      {product.price.toLocaleString("id-ID", {
-                        style: "currency",
-                        currency: "IDR",
-                        minimumFractionDigits: 0,
-                      })}
-                    </td>
-                    <td>{item.qty}</td>
-                    <td>
-                      {(item.qty * product.price).toLocaleString("id-ID", {
-                        style: "currency",
-                        currency: "IDR",
-                        minimumFractionDigits: 0,
-                      })}
-                    </td>
-                  </tr>
-                );
-              })}
+              {products.length > 0 &&
+                cart.map((item) => {
+                  const product = products.find((p) => p.id === item.id);
+                  return (
+                    <tr key={item.id}>
+                      <td>{product.title}</td>
+                      <td>
+                        {product.price.toLocaleString("en-US", {
+                          style: "currency",
+                          currency: "USD",
+                          minimumFractionDigits: 0,
+                        })}
+                      </td>
+                      <td>{item.qty}</td>
+                      <td>
+                        {(item.qty * product.price).toLocaleString("en-US", {
+                          style: "currency",
+                          currency: "USD",
+                          minimumFractionDigits: 0,
+                        })}
+                      </td>
+                    </tr>
+                  );
+                })}
               <tr ref={totalPriceRef}>
                 <td colSpan={3} className="font-bold">
                   Total Price
                 </td>
                 <td className="font-bold">
-                  {totalPrice.toLocaleString("id-ID", {
+                  {totalPrice.toLocaleString("en-US", {
                     style: "currency",
-                    currency: "IDR",
+                    currency: "USD",
                     minimumFractionDigits: 0,
                   })}
                 </td>
